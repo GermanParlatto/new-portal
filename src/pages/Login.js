936bf471875fd48f9891f822dedf4b74f63a06a3
German Parlatto <german.parlatto@autoremind.com>
2022-07-06 03:51:09 +0200

    change title login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -62,7 +62,7 @@ export default function Login() {
   const mdUp = useResponsive('up', 'md');
 
   return (
-    <Page title="Login">
+    <Page title="Login main">
       <RootStyle>
         <HeaderStyle>
           <Logo />
